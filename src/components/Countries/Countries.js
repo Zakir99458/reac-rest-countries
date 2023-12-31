ed57d232a915f62dfa5275c02fe1ed484d07a45a
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-31 20:57:54 +0200

    components

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>From separate components file</h2>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <p>This is person components</p>
+        </div>
+    );
+};
+
+export default Person;
